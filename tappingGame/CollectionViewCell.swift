d1729531c145f065fc424edc442e30ad009e3e77
tonyji146@gmail.com <tonyji146@gmail.com>
2021-03-24 23:26:51 +0530

    first commit

---
+++ tappingGame/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  tappingGame
+//
+//  Created by Tonywilson Jesuraj on 24/03/21.
+//  Copyright © 2021 Tonywilson Jesuraj. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
